0072a91d6c1be6db548028bb929096541e9632e2
barriolo <matheusbarriolo@gmail.com>
2019-12-10 20:10:00 -0300

    git ignore

--- src/app/moviedb-config.ts
+++
@@ -1,5 +0,0 @@
-export class ApiMoviedb {
-  apiKey: string = '64ff6f0314eadaf517aae045984f8f18';
-  baseUrl: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${this.apiKey}&language=pt-Br&page=1`;
-  buscaCep: string = 'https://viacep.com.br/ws/';
-}
