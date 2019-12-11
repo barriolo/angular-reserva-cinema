1bf607792a74e9b4e687609b5e47d1ddb87ae7d5
barriolo <matheusbarriolo@gmail.com>
2019-12-10 21:05:47 -0300

    criação de models

--- src/app/moviedb-config-sample.ts
+++ src/app/moviedb-config-sample.ts
@@ -1,5 +1,6 @@
 export class ApiMoviedb {
   apiKey: string = 'api key here';
   baseUrl: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${this.apiKey}&language=pt-Br&page=1`;
+  buscaCep: string = 'https://viacep.com.br/ws/';
 }
 
