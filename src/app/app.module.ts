ddbd07f4c9dbf956ac2c1ac86ef66d95687027b2
barriolo <matheusbarriolo@gmail.com>
2019-12-11 21:28:44 -0300

    bind de titulo e imagens no card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UpcomingService } from './core/services/upcoming.service';
 import { HttpClientModule } from '@angular/common/http';
 import { BuscarCepService } from './core/services/buscar-cep.service';
-import { EnderecoComponent } from './shared/components/forms/endereco/endereco.component';
-import { AcompanhanteComponent } from './shared/components/forms/acompanhante/acompanhante.component';
-import { MovieCardComponent } from './shared/components/cards/movie-card/movie-card.component';
 import { UpcomingModule } from './modules/upcoming/upcoming.module';
+import { CardsModule } from './shared/components/cards/cards.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EnderecoComponent,
-    AcompanhanteComponent,
-    MovieCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    UpcomingModule
+    UpcomingModule,
+    CardsModule,
+    FlexLayoutModule
   ],
   providers: [
     UpcomingService,
--- src/app/modules/upcoming/upcoming.module.ts
+++ src/app/modules/upcoming/upcoming.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ListMoviesUpcomingComponent } from './pages/list-movies-upcoming/list-movies-upcoming.component';
 import { ReserveMovieUpcomingComponent } from './pages/reserve-movie-upcoming/reserve-movie-upcoming.component';
 import { UpcomingService } from 'src/app/core/services/upcoming.service';
+import { CardsModule } from 'src/app/shared/components/cards/cards.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
 @NgModule({
   declarations: [ListMoviesUpcomingComponent, ReserveMovieUpcomingComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CardsModule,
+    FlexLayoutModule
   ],
   exports: [
     ListMoviesUpcomingComponent
